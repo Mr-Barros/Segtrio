a012a6a0b6f538fca278226bae1861e2d8e67d69
Davi Martignoni Barros <davimartignonibarros@gmail.com>
2026-01-26 00:55:50 -0300

    modify gen.cpp

--- Extra/gen.cpp
+++ Extra/gen.cpp
@@ -1,44 +1,21 @@
 #include <bits/stdc++.h>
-using namespace std;
-
 typedef long long ll;
-typedef pair<ll, ll> p64;
-typedef vector<ll> v64;
-
-#define forn(i, s, e) for (ll i = (s); i < (e); i++)
+using namespace std;
 #define ln '\n'
-#define sz(x) (ll) x.size()
-
-const ll INF = 0x3f3f3f3f3f3f3f3fll;
-
-// #define DEBUG
-#if defined(DEBUG)
-    void dbg_out() { cerr << endl; }
-    template<typename Head, typename... Tail>
-    void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
-    #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__) 
-#else
-    #define dbg(...) (void) 0
-#endif
 
 mt19937_64 rng((ll) chrono::steady_clock::now().time_since_epoch().count());
 
-ll uniform(ll l, ll r){
+ll uniform(ll l, ll r) {
     uniform_int_distribution<ll> uid(l, r);
     return uid(rng);
 }
 
-void solve() {
-    ll n = uniform(0, 10);
-    cout << n << ln;
-}
+const ll MINN = 1;
+const ll MAXN = 1e6;
 
 signed main(void) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ll n = uniform(MINN, MAXN);
+    cout << n << ln;
 
-    ll tc = 1;
-    // cin >> tc;
-    while (tc--) solve();
     return 0;
 }
