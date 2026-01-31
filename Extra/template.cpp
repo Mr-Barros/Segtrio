bc6e17876bc0a20ddf6a0b35a6c140612a5a58c9
Bruno Azambuja Carvalho <76236184+brunoazca@users.noreply.github.com>
2026-01-31 14:46:26 -0300

    Update template.cpp

--- Extra/template.cpp
+++ Extra/template.cpp
@@ -17,10 +17,12 @@ const ll INF = 0x3f3f3f3f3f3f3f3fll;
     template<typename Head, typename... Tail>
     void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
     #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__) 
-#else
-    #define dbg(...) (void) 0
     #define dbgv(v) cerr << "(" #v "): "; for (auto x : v) cerr << x << " "; cerr << ln;
     #define dbgg(g) cerr << "(" #g "): \n"; forn(i,0,sz(g)){ cerr << i << ": "; for(auto x:g[i]) cerr << x << " " ; cerr << '\n';}
+#else
+    #define dbg(...) (void) 0
+    #define dbgv(...) (void) 0
+    #define dbgg(...) (void) 0
 #endif
 
 void solve() {
