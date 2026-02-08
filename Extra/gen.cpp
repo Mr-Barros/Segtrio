fe7a0a7cba760e6b9c40c33f292dbea861017b55
Davi Barros <121593633+Mr-Barros@users.noreply.github.com>
2026-02-07 23:39:27 -0300

    update template for gen.cpp

--- Extra/gen.cpp
+++ Extra/gen.cpp
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
-typedef long long ll;
 using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> p64;
+typedef vector<ll> v64;
+
+#define forn(i, s, e) for (ll i = (s); i < (e); i++)
 #define ln '\n'
+#define sz(x) (ll) x.size()
+
+const ll INF = 0x3f3f3f3f3f3f3f3fll;
 
 mt19937_64 rng((ll) chrono::steady_clock::now().time_since_epoch().count());
 
@@ -10,11 +18,10 @@ ll uniform(ll l, ll r) {
     return uid(rng);
 }
 
-const ll MINN = 1;
 const ll MAXN = 1e6;
 
 signed main(void) {
-    ll n = uniform(MINN, MAXN);
+    ll n = uniform(1, MAXN);
     cout << n << ln;
 
     return 0;
